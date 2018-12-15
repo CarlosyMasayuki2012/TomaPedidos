565edf324b83767abb1e5eb995130083bfa7fd73
CarlosyMasayuki2012 <carlos.miyashiro.tsukazan@outlook.com>
2018-12-15 11:52:41 -0500

    Primer Commit

--- app/src/main/java/com/example/sistemas/tomapedidos/BusquedaClienteActivity.java
+++ app/src/main/java/com/example/sistemas/tomapedidos/BusquedaClienteActivity.java
@@ -7,18 +7,23 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class BusquedaClienteActivity extends AppCompatActivity {
 
     RadioGroup rggrupocliente;
     RadioButton rbnombre,rbcodigo;
     Button btnbuscar;
+    ArrayList<String> listaClientes;
 
     ListView lvclientes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_busqueda_cliente);
 
         rggrupocliente = findViewById(R.id.rgBuscarCliente);
         rbnombre = findViewById(R.id.rbNombre);
@@ -26,22 +31,22 @@ public class BusquedaClienteActivity extends AppCompatActivity {
         btnbuscar = findViewById(R.id.btnBuscar);
         lvclientes = findViewById(R.id.lvClientes);
 
-    }
 
-    public void onRadioButtonClicked(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
 
-        // Check which radio button was clicked
-        switch(view.getId()) {
-            case R.id.rbNombre:
-                if (checked)
-                    // Pirates are the best
-                    break;
-            case R.id.rbCodigo:
-                if (checked)
-                    // Ninjas rule
-                    break;
-        }
+        rggrupocliente.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (rggrupocliente.getCheckedRadioButtonId()){
+
+                    case R.id.rbNombre:
+                        Toast.makeText(getApplicationContext(), "Check nombre", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.rbCodigo:
+                        Toast.makeText(getApplicationContext(), "Check Codigo", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
+
     }
 }
